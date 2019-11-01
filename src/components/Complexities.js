d9fcccd8534be989287dd792f351548306a0705f
sorc97 <nnd64809200@gmail.com>
2019-11-01 13:00:17 +0300

    Style editing and Phrases component changing to component without the state

--- src/components/Complexities.js
+++ src/components/Complexities.js
@@ -3,20 +3,20 @@ import Complexity from './Complexity'
 
 const Complexitites = ({words = [], onChange = f => f, onSetContext = f => f}) =>
   <div className='complexity-wrapper'>
-    <Complexity title = 'hard' 
-      onChange = {onChange}
-      onSetContext = {onSetContext}
-      words = {words}
+    <Complexity title='hard' 
+      onChange={onChange}
+      onSetContext={onSetContext}
+      words={words}
     />
-    <Complexity title = 'medium' 
-      onChange = {onChange}
-      onSetContext = {onSetContext}
-      words = {words}
+    <Complexity title='medium' 
+      onChange={onChange}
+      onSetContext={onSetContext}
+      words={words}
     />
-    <Complexity title = 'easy' 
-      onChange = {onChange}
-      onSetContext = {onSetContext}
-      words = {words}
+    <Complexity title='easy' 
+      onChange={onChange}
+      onSetContext={onSetContext}
+      words={words}
     />
   </div>
 
--- src/components/PhrasesModal.js
+++ src/components/PhrasesModal.js
@@ -1,66 +1,42 @@
 import React from 'react'
 import AddPhraseForm from './AddPhraseForm'
 import PhrasesList from './PhrasesList'
-import PropTypes from 'prop-types'
 import {withRouter} from 'react-router-dom'
 
-class PhrasesModal extends React.Component{
-  constructor() {
-    super();
 
-    this.hideModal = this.hideModal.bind(this);
-  }
+const PhrasesModal = ({word = {}, onNewPhrase = f=> f, history}) => {
 
-  componentDidUpdate() {
-    console.log("Modal was UPDATE");
-  }
-
-  hideModal(e){
+  const hideModal = e => {
     let target = e.target;
-    const {history} = this.props;
 
     if(!target.classList.contains("phrases-modal")) return;
 
     history.goBack();
   }
 
-  render() {
-    const {word, onNewPhrase, history} = this.props;
-
-    return(
-      <div 
-        className='phrases-modal'
-        onClick={this.hideModal}
-      >
-        <div className="phrases-content">
-          <h1><span>{word.eng}</span> <br/> <span>{word.rus}</span></h1>
-          <div onClick={()=> history.goBack()} 
-            className="phrases-content-close">
-              &times;
-          </div>
-          <AddPhraseForm 
-            onNewPhrase={phrase => onNewPhrase(phrase, word.id)}/>
-          {
-            (!Object.keys(word).length) ?
-              "" :
-              (!word.phrases.length) ? 
-                <p>No phrases in the list</p> :
-                <PhrasesList phrases={word.phrases}/>
-          }
+  return(
+    <div 
+      className='phrases-modal'
+      onClick={hideModal}
+    >
+      <div className="phrases-content">
+        <h1><span>{word.eng}</span> <br/> <span>{word.rus}</span></h1>
+        <div onClick={()=> history.goBack()} 
+          className="phrases-content-close">
+            &times;
         </div>
+        <AddPhraseForm 
+          onNewPhrase={phrase => onNewPhrase(phrase, word.id)}/>
+        {
+          (!Object.keys(word).length) ?
+            "" :
+            (!word.phrases.length) ? 
+              <p>No phrases in the list</p> :
+              <PhrasesList phrases={word.phrases}/>
+        }
       </div>
-    )
-  }
-}
-
-PhrasesModal.propTypes = {
-  word: PropTypes.object,
-  onNewPhrase: PropTypes.func
-}
-
-PhrasesModal.defaultProps = {
-  word: {},
-  onNewPhrase: f => f
+    </div>
+  )
 }
   
 export default withRouter(PhrasesModal)
