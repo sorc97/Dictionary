c2bdec3563cf3ebe1fa0a3e094aad4953c9b311c
sorc97 <nnd64809200@gmail.com>
2020-02-01 23:45:12 +0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Dictionary
-## About
-This app allows you to create a card of word, which you can place in three sections of complexity (Easy, Medium, Hard), you also can choose in what kind of language words will display. You can call the context menu on each card of word and add some context phrases, for better remembering of word, in this menu you can also remove word or place him in the new section.
---
+++ precache-manifest.98e821cae5a3c2dd531b0e74f3ad55bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8dc616112c100fb6ec53e0467d5417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9640eba3fba96bde717",
+    "url": "/static/css/main.3950eab3.chunk.css"
+  },
+  {
+    "revision": "3d23095671844042aff0",
+    "url": "/static/js/2.130d72f4.chunk.js"
+  },
+  {
+    "revision": "d9640eba3fba96bde717",
+    "url": "/static/js/main.54cadd7f.chunk.js"
+  },
+  {
+    "revision": "1c92dff94f7f8d9135f4",
+    "url": "/static/js/runtime-main.a29b7788.js"
+  }
+]);
